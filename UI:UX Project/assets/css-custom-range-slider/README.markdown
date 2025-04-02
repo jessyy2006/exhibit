eb200ac64f23f507e09291afdffe31fdff5a44a0
jessyy2006 <worldlive145@gmail.com>
2025-04-02 17:20:01 -0400

    Add files via upload

---
+++ UI:UX Project/assets/css-custom-range-slider/README.markdown
@@ -0,0 +1,4 @@
+# CSS Custom Range Slider
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/brandonmcconnell/pen/oJBVQW](https://codepen.io/brandonmcconnell/pen/oJBVQW).
+
+ 
---
+++ UI:UX Project/iterations/script.js
@@ -0,0 +1,10 @@
+
+// Slider
+$(function() {
+	var rangePercent = $('[type="range"]').val();
+	$('[type="range"]').on('change input', function() {
+		rangePercent = $('[type="range"]').val();
+		$('h4').text(rangePercent);
+		$('[type="range"]').css('filter', 'hue-rotate(-' + rangePercent + 'deg)');
+	});
+});
